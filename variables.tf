19edc9743ec615f93395d920a5d72dbb9a87c377
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2024-10-18 16:12:43 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+#resource_group
+variable "resource_group_name" {
+    type = string
+    description = "resource_group_name"
+}
+
+variable "location" {
+    type = string
+    description = "location"
+}
